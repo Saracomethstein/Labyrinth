d7f040f5837a315b2f95fde182cca5b8c954eeba
Saracomethstein <adel.sevastianov@yandex.ru>
2023-10-06 15:06:56 +0300

    Update file and first run

--- Models/RelayCommand.cs
+++ Models/RelayCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labyrinth.Models
+﻿namespace Labyrinth.Models
 {
     internal class RelayCommand
     {
